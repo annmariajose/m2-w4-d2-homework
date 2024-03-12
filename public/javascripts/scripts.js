b799317c89386b9659309926358ecb93e645c075
Ann Maria Jose <annmariajose1412@gmail.com>
2024-03-12 16:51:14 -0500

    Change food blog to express app

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -49,7 +49,7 @@ var vm1 = new Vue({
         foodieTitle: "Foodie Level: ",
         bioTitle: "Bio: ",
         post1: {
-            profileImage: "profile.png",
+            profileImage: "images/profile.png",
             author: "Brianna",
             foodieLevel: "Novice",
             bio: "Food enthusiast. Love to cook and experiment. Into only organic, fat free, sugar free stuffs!",
@@ -57,7 +57,7 @@ var vm1 = new Vue({
             review: "Was amazing! My Walmart didn’t have coriander in stock and didn’t have ground cumin. I used serrano instead of jalapeño. It was just like my favorite tortilla soup from BJs. I am sending this recipe to my family. I want everyone to try it!"
         },
         post2: {
-            profileImage: "profile.png",
+            profileImage: "images/profile.png",
             author: "LINH",
             foodieLevel: "Newcomer",
             bio: "Love food! Grew up with meat and potatoes. Recently venture outside my comfort zone. Loving everything I have been eating so far. Thai is my favorite at this time.",
@@ -65,7 +65,7 @@ var vm1 = new Vue({
             review: "I just made this soup today and it’s so tasty! didn’t have corn at home but still turned out very good. It’s a winner! I made beef chili for my parents; but since my dad has gout he can’t eat beef; this white chicken chili is perfect for him. Thank you Lisa!"
         },
         post3: {
-            profileImage: "profile.png",
+            profileImage: "images/profile.png",
             author: "CATHERINE LEONARDO",
             foodieLevel: "Mentor",
             bio: "I have to say I never was the adventurous type until 2 years ago. My boyfriend, who is of Japanese background, exposed me to other cultural food and I have never look back since!",
@@ -73,7 +73,7 @@ var vm1 = new Vue({
             review: "I LOVE this White Chicken Chili! You are right, it is satiating meal—delicious with toasted bread. Refreshingly different taste than any chicken chili I’ve made in the past. I made it exactly as written and added some chopped zucchini, carrots, and celery. Instead of shredding the chicken, I chopped it into small pieces. It freezes very well. Will be an all-time favorite, for sure."
         },
         post4: {
-            profileImage: "profile.png",
+            profileImage: "images/profile.png",
             author: "KALI",
             foodieLevel: "Novice",
             bio: "Food is my passion. So is cooking. I love to experiment and try new things. I have to admit I'm a food whore! Invite me over for dinner and I'll be there!",
